dcbad8acae236ee93f6441f5b3df2155a10beddd
jolsefu <jules.leo.reserva@gmail.com>
2024-08-09 20:48:19 +0800

    Changed warning prompt on mobile devices

--- main.js
+++ main.js
@@ -341,12 +341,12 @@ function correctOrientation() {
 	if ( window.matchMedia( "(orientation: portrait)" ).matches ) {
 		const div = document.createElement( 'div' );
 		const button = document.createElement( 'button' );
-		const warning = '<div>This website is best experienced in landscape. Click the button after reorientating.</div>';
+		const warning = '<div style="width: 50vh;">This website is best experienced in a laptop or computer. If you do still wish to continue, orient it to landscape. Click the button after reorientating.</div>';
 
 		button.onclick = () => { window.location.reload( true ) };
 		button.innerHTML = 'Reload page';
 
-		div.setAttribute( 'style', 'display: flex; height: 50vh; justify-content: center; align-items: center; flex-direction: column;' );
+		div.setAttribute( 'style', 'display: flex; height: 50vh; justify-content: center; align-items: center; flex-direction: column; margin: 1rem;' );
 		div.innerHTML = warning;
 		div.appendChild( button );
 
